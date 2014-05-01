47b321a842627c9e80e1629e7165f1d8768d5a25
oMicrone <omicrone@gmail.com>
2014-05-01 12:35:46 +0200

    Added Minified version and breakpoint file .. you don't need it, you can set responsive elements by adding them data-responsive_element attribute

---
+++ responsive-breakpoint.js
@@ -0,0 +1,11 @@
+var responsive_breakpoints = [
+	{
+		select: '#container',
+		breakpoints: 'small:320, middle:640, standard:980, big:1400, huge'
+	},
+	{
+		select: '#container > header',
+		breakpoints: 'compact:640, standard'
+	}
+	
+];
